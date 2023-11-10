58c977c8b98ccbba892e599fed7f42ac8c46ac49
Md Main Uddin <2021md.main@gmail.com>
2023-11-10 22:55:07 +0600

    error handlers added

---
+++ src/app.ts
@@ -0,0 +1,46 @@
+import cors from 'cors';
+import express, { Application, NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+// import config from './utils/config';
+// import router from './route';
+
+const app: Application = express();
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
+app.use('*', cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(config.api_route as string, router);
+
+app.use('/test', (req, res) => {
+  const message = `Server is running ${new Date()}`;
+  res.status(StatusCodes.OK).json({
+    message,
+  });
+});
+
+app.get('', (req, res) => {
+  const message = `Server is running `;
+  res.status(StatusCodes.OK).json({
+    message,
+  });
+});
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: 'Not found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API not found',
+      },
+    ],
+  });
+  next();
+});
+
+export default app;
